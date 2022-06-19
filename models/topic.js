efd063ab485ab6be00e2fde1a4d119c0596a547a
kanishk Garg <shagungarg10012002@gmail.com>
2022-06-19 13:34:26 +0530

    first-commit

---
+++ models/topic.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+
+var topicSchema = new mongoose.Schema({
+    topicName: String,
+    performance: [{
+        username: String,
+        marks: Number
+    }],
+    marks: Number,
+    passMarks: Number,
+    time: Number,
+    mcqs: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'MCQ'
+    }]
+});
+
+module.exports = mongoose.model('Topic', topicSchema);
---
+++ models/user.js
@@ -0,0 +1,33 @@
+var mongoose = require('mongoose');
+var passportLocalMongoose = require('passport-local-mongoose');
+
+var userSchema = new mongoose.Schema({
+    type: { type: String },
+    tmarks:Number,
+    username: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        minlength: 4,
+        trim: true
+    },
+    branch: {
+        type: String,
+        required: true
+    },
+    subject: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Subject'
+        }
+    ],
+    year: String,
+    type: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', userSchema);
